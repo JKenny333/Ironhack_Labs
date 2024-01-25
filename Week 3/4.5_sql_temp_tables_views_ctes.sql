0f17e14296bdb855af21fc0abc78476d0ecc3fc0
JKenny333 <155671756+JKenny333@users.noreply.github.com>
2024-01-25 15:32:30 +0100

    update

--- Week 3/4.5_sql_temp_tables_views_ctes.sql
+++ Week 3/4.5_sql_temp_tables_views_ctes.sql
@@ -126,8 +126,43 @@ LIMIT 20;
 
 -- ==================================================
 
+-- ================================
+-- Code along from alex examples
+
+CREATE VIEW top5_movies AS
+SELECT title, COUNT(rental_id)
+FROM film
+INNER JOIN inventory
+USING(film_id)
+INNER JOIN rental
+USING(inventory_id)
+GROUP BY title
+ORDER BY COUNT(rental_id) DESC
+LIMIT 5;
+
+SELECT * FROM top5_movies;
 
 
+#2 examples of CTEs:
+#1
+WITH average_spent AS (SELECT AVG(total_spent) avg_total_spent
+							FROM   (SELECT SUM(amount) AS total_spent
+									FROM payment
+									GROUP BY customer_id) AS tspc)
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+HAVING SUM(amount) > (SELECT avg_total_spent
+						FROM average_spent);
+#2
+WITH RegionalSales AS (
+    SELECT region, SUM(amount) AS total_sales
+    FROM Sales
+    GROUP BY region
+)
+SELECT region, total_sales
+FROM RegionalSales
+WHERE total_sales > 100000;
 
 
 
